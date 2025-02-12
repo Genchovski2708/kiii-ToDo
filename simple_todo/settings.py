3f8a6b1774b26316b9854cc5c105686bdb23095d
Genchovski2708 <andrej_geno@outlook.com>
2025-02-12 07:03:56 +0100

    docker-compose.yml, ci.yml

--- simple_todo/settings.py
+++ simple_todo/settings.py
@@ -74,11 +74,19 @@ WSGI_APPLICATION = 'simple_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import os
+import environ
+import dj_database_url
+
+# Initialize environment variables
+env = environ.Env()
+environ.Env.read_env()
+
+# Database Configuration
+DATABASE_URL = os.getenv('DATABASE_URL')
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=DATABASE_URL)
 }
 
 
